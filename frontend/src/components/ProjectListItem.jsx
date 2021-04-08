97d62c615b92e7635c8384a0c508906eba165a48
anders <andersbjorklund@live.com>
2021-04-08 20:30:13 +0200

    WIP: remove everything about edit state
    Remove everything that has anything to with editing state.
    This component will be switch to using a modal to edit name instead.

--- frontend/src/components/ProjectListItem.jsx
+++ frontend/src/components/ProjectListItem.jsx
@@ -1,48 +1,14 @@
-import React, { Fragment, useState, useContext } from 'react';
+import React from 'react';
 import {
   ListItem,
   ListItemText,
-  TextField,
 } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import PropTypes from 'prop-types';
-import { AuthContext } from '../contexts/AuthContext';
-import { ProjectContext } from '../contexts/ProjectContext';
-import ProjectApiWrapper from '../api/Project';
 
-const ProjectListItem = ({ project, onClick, onDeleteIconClick, index, selected }) => {
-  const [editingState, setEditingState] = useState(false);
-  const [projectName, setProjectName] = useState('');
-
-  const { authContext } = useContext(AuthContext);
-  const { projectContextAPI } = useContext(ProjectContext);
-
-  const projectApiWrapper = new ProjectApiWrapper(authContext.token);
-
-  const onEditIconClick = () => {
-    setEditingState(true);
-    setProjectName(project.name);
-  };
-
-  const onEditProjectFormSubmit = async (e, id, name) => {
-    e.preventDefault();
-    await projectApiWrapper.editProject(id, name)
-      .catch((e) => console.log(e));
-
-    projectContextAPI.editProject(id, name);
-    setEditingState(false);
-  };
-
-  const editState = (
-    <ListItem>
-      <form onSubmit={async (e) => await onEditProjectFormSubmit(e, project.id, projectName)}>
-        <TextField id="editProjectInput" label="Standard" value={projectName} onChange={(e) => setProjectName(e.target.value)} />
-      </form>
-    </ListItem>
-  );
-
-  const normalState = (
+const ProjectListItem = ({ project, onClick, onEditIconClick, onDeleteIconClick, index, selected }) => {
+  return (
     <ListItem
       button
       selected={selected}
@@ -54,15 +20,9 @@ const ProjectListItem = ({ project, onClick, onDeleteIconClick, index, selected 
       <EditIcon id="editProjectButton" onClick={() => onEditIconClick()} />
       <DeleteIcon
         id="deleteProjectButton"
-        onClick={async () => await onDeleteIconClick(project.id)}
+        onClick={() => onDeleteIconClick()}
       />
     </ListItem>
-  );
-
-  return (
-    <Fragment>
-      {editingState ? (editState) : (normalState)}
-    </Fragment>
   );
 };
 
