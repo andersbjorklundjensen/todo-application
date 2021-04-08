47d217fa612cdc955e75dc4756b4d04ff27f8356
anders <andersbjorklund@live.com>
2021-04-08 20:45:33 +0200

    Add edit modal for project list items

--- frontend/src/components/ProjectList.jsx
+++ frontend/src/components/ProjectList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import {
   List,
 } from '@material-ui/core';
@@ -6,8 +6,28 @@ import ProjectListItem from '../components/ProjectListItem';
 import { ProjectContext } from '../contexts/ProjectContext';
 import { AuthContext } from '../contexts/AuthContext';
 import ProjectApiWrapper from '../api/Project';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const ProjectList = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
   const { projectContext, projectContextAPI } = useContext(ProjectContext);
   const { authContext } = useContext(AuthContext);
 
@@ -27,14 +47,34 @@ const ProjectList = () => {
   return (
     <List id="projectList" component="div" disablePadding>
       {projectContext.projects && projectContext.projects.map((project, index) => (
-        <ProjectListItem
-          onClick={() => onProjectItemClick(project.id, project.name)}
-          onEditIconClick={() => {}}
-          onDeleteIconClick={() => onDeleteIconClick(project.id)}
-          selected={projectContext.currentProject === project.id}
-          key={index}
-          project={project}
-        />
+        <Fragment>
+          <ProjectListItem
+            onClick={() => onProjectItemClick(project.id, project.name)}
+            onEditIconClick={() => setOpen(true)}
+            onDeleteIconClick={() => onDeleteIconClick(project.id)}
+            selected={projectContext.currentProject === project.id}
+            key={index}
+            project={project}
+          />
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            className={classes.modal}
+            onClose={() => setOpen(false)}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">{project.name}</h2>
+              </div>
+            </Fade>
+          </Modal>
+        </Fragment>
       ))}
     </List>
   )
